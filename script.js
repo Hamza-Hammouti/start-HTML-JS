8a58fb114c7701a0e498a6eb3705e9e64c56b409
HamzaHammouti <hamzahhammouti@gmail.com>
2022-03-31 11:07:11 +0200

    Lightswitch werkend in JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Switch light off';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+var licht = "aan";
+btn.onclick = aanenuit
 
-
+function aanenuit() {
+    if (licht == "aan") {
+        console.log("Het licht is aan");
+        document.body.style.backgroundColor = "yellow";
+        btn.innerHTML = 'Light switch is on';
+        licht = "uit"
+    } else {
+        console.log("Het licht is uit");
+        document.body.style.backgroundColor = "black";
+        btn.innerHTML = 'Light switch is off';
+        licht = "aan"
+    }
+}
 
 // schijf hier tussen je code
